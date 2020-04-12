6098ebc18b49a843486e6ff33192dcd3399f88da
JanHovland <jan.hovland@lyse.net>
2020-04-12 16:42:50 +0200

    Update

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -7,22 +7,30 @@
 //
 
 import SwiftUI
+import WebKit
+
+struct TestView : NSViewRepresentable {
+    func makeNSView(context: Context) -> WKWebView  {
+        let view = WKWebView()
+        if let url = URL(string: "https://www.google.com/") {
+            view.load(URLRequest(url: url))
+        }
+        return view
+    }
+
+    func updateNSView(_ view: WKWebView, context: Context) {
+    }
+
+}
+
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(1 ... 10, id: \.self) { index in
-                    NavigationLink(destination: DetailView(value: index)) {
-                        Text("Link \(index)")
-                    }
-                }
-
-            }
-        }
-        .listStyle(SidebarListStyle())
+        TestView()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -30,11 +38,36 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct DetailView: View {
-    var value: Int
-    
-    var body: some View {
-        Text("Detail Link for index " + String(value))
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-    }
-}
+
+//import SwiftUI
+//
+//struct ContentView: View {
+//    var body: some View {
+//        NavigationView {
+//            List {
+//                ForEach(1 ... 10, id: \.self) { index in
+//                    NavigationLink(destination: DetailView(value: index)) {
+//                        Text("Link \(index)")
+//                    }
+//                }
+//
+//            }
+//        }
+//        .listStyle(SidebarListStyle())
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
+//
+//struct DetailView: View {
+//    var value: Int
+//
+//    var body: some View {
+//        Text("Detail Link for index " + String(value))
+//            .frame(maxWidth: .infinity, maxHeight: .infinity)
+//    }
+//}
